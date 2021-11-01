6e3746eb504728ce3926292563b9928bbe7da877
Mangobats <mylze.arnold@gmail.com>
2021-11-01 20:46:55 +0800

    added database migrations

--- smartcare_app/database/migrations/2014_10_12_000000_create_users_table.php
+++ smartcare_app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('ssn');
+            $table->string('contact');
+            $table->tinyInteger('age');
             $table->rememberToken();
             $table->timestamps();
         });
