93523165bd6774ca9254e60e8a44a541c14c8a2c
therealmai <jomar35leano@gmail.com>
2021-12-05 01:06:22 +0800

    header file

--- public/homepage.php
+++ public/homepage.php
@@ -21,76 +21,8 @@
 </head>
 <body>
 
-<div class="container-nav">
+<?php include('header.php') ?>
  
- 
-    <nav class="navbar navbar-expand-lg">
-    
-      <div class="container-xl">  
- 
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-            <div class="logo-image">
-                  <img src="../src/img/logo.jpg" width="100" height="300" class="img-fluid">
-            </div>
-            </a>
-        
-           
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse nav justify-content-end" id="navbarNavDropdown">
-            <ul class="nav">
-
-              <li class="nav-item">
-                <a class="nav-link nav-head" href="#">Home</a>
-              </li>
-
-              <li class="nav-item dropdown nav-head">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  About Us
-                </a>
-                <!-- <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul> -->
-              </li>
-
-              <li class="nav-item dropdown nav-head">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                 Doctors
-                </a>
-                <!-- <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul> -->
-              </li>
-
-              <li class="nav-item dropdown nav-head">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Departments
-                </a>
-                <!-- <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul> -->
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link nav-head" href="#">Services</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link nav-head" href="#">Contacts</a>
-              </li>
-
-            </ul>
-          </div>
-        </div>
-      </div>
-      </nav>
     <main>
         <section class="main-sectinon-1">
             <!-- start of Card -->
