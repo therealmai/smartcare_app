b5b473c127aa2b610af572a5e2478251738f3238
therealmai <jomar35leano@gmail.com>
2021-12-08 21:36:14 +0800

    navbar

--- public/header.php
+++ public/header.php
@@ -36,7 +36,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Navbar brand -->
                 <a class="navbar-brand mt-2 mt-lg-0" href="homepage.php">
-                    <img src="../src/img/logo-with-heading.png" height="50"loading="lazy" />
+                    <img src="../src/img/logo-with-heading.png" height="50" loading="lazy" />
                 </a>
                 <!-- Left links -->
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -46,9 +46,16 @@
                     <li class="nav-item">
                         <a class="nav-link" href="search.php">Search</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="profile-doc.php">Profile</a>
-                    </li> 
+                    <?php if ($_SESSION['currUser']['role'] == "doctor") { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="profile-doc.php">Profile Doctor</a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="profile-pat.php">Profile Patient</a>
+                        </li>
+                    <?php  } ?>
+
                     <li class="nav-item">
                         <a class="nav-link" href="prescriptions.php">Prescriptions</a>
                     </li>
@@ -59,15 +66,15 @@
 
             <!-- Right elements -->
             <div class="d-flex align-items-center">
-             <button class="btn btn-primary btn-sm" type="button"> <a href="../src/php/logout.php" style="color: white; text-decoration: none;"> Logout</a></button>
+                <button class="btn btn-primary btn-sm" type="button"> <a href="../src/php/logout.php" style="color: white; text-decoration: none;"> Logout</a></button>
             </div>
             <!-- Right elements -->
         </div>
         <!-- Container wrapper -->
     </nav>
     <!-- Navbar -->
-       
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
