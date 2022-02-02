a18aa2658f681265992e342d073274e841492602
therealmai <jomar35leano@gmail.com>
2022-02-02 23:36:27 +0800

    cleaned design

--- public/patients.php
+++ public/patients.php
@@ -24,47 +24,66 @@ include '../src/php/dbconnect.php';
 
 <body>
 
-    <?php 
-        include "./header.php";
-        
-        $docSql = "SELECT * FROM `doctors` WHERE `userID` = {$_SESSION['currUser']['id']}";
-        $docResults = mysqli_query($mysqli, $docSql);
-        $doc = mysqli_fetch_assoc($docResults);
+    <?php
+    include "./header.php";
+
+    $docSql = "SELECT * FROM `doctors` WHERE `userID` = {$_SESSION['currUser']['id']}";
+    $docResults = mysqli_query($mysqli, $docSql);
+    $doc = mysqli_fetch_assoc($docResults);
     ?>
     <main class="profPres">
+
+        <!-- <table id="patientsTable" class="display">
+            <thead>
+                <tr>
+                    <th>Column 1</th>
+                    <th>Column 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Row 1 Data 1</td>
+                    <td>Row 1 Data 2</td>
+                </tr>
+                <tr>
+                    <td>Row 2 Data 1</td>
+                    <td>Row 2 Data 2</td>
+                </tr>
+            </tbody>
+        </table> -->
         <section>
             <h1>Patients</h1>
             <div class="table-over">
 
 
-            <table style="width:100%;" id='patientsTable' class="display">
-                <col span="1" style="width: 30%;">
-                <col span="1" style="width: 45%;">
-                <col span="1" style="width: 15%;">
-                <col span="1" style="width: 10%;">
-                <thead>
-                    <tr>
-                        <th>
-                            Patient
-                        </th>
-                        <th>
-                            Prescription
-                        </th>
-                        <th>
-                            Lab Test
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
+                <table style="width:100%;" id='patientsTable' class="display">
+                    <col span="1" style="width: 33%;">
+                <col span="1" style="width: 33%;">
+                <col span="1" style="width: 33%;">
+                <!-- <col span="1" style="width: 10%;"> -->
+                    <thead>
+                        <tr>
+                            <th>
+                                Patient
+                            </th>
+                            <th>
+                                Prescription
+                            </th>
+                            <th>
+                                Lab Test
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-                    <?php
+                        <?php
                         $patientsSql = "SELECT `u`.`firstname`,`u`.`lastname`,`u`.`middle_initial`,`p`.* 
                             FROM `patients` `p`
                             LEFT JOIN `users` `u` ON `p`.`userID` = `u`.`id`";
 
                         $patientsResults = mysqli_query($mysqli, $patientsSql);
                         $patientsRows = mysqli_fetch_all($patientsResults, MYSQLI_ASSOC);
-                    
+
 
                         // var_dump($_SESSION);
                         // exit();
@@ -88,83 +107,83 @@ include '../src/php/dbconnect.php';
 
                         if (count($patientsRows)) {
 
-                            foreach ($patientsRows AS $patient) {
+                            foreach ($patientsRows as $patient) {
                                 // var_dump($patient, $_SESSION);
                                 // exit();
                                 // $formattedDate = date('m/d/Y', strtotime($prescriptionRow['date']));
-                                ?>
-                                    <tr>
-                                        <td>
-                                            <?php echo "{$patient['firstname']} ".
-                                                    $patient['middle_initial'] ?? ''
-                                                    ."{$patient['lastname']}"; ?>
-                                        </td>
+                        ?>
+                                <tr>
+                                    <td>
+                                        <?php echo "{$patient['firstname']} " .
+                                            $patient['middle_initial'] ?? ''
+                                            . "{$patient['lastname']}"; ?>
+                                    </td>
 
-                                        <td>
+                                    <td>
                                         <?php
-                                            $prescriptionsSql = "SELECT * FROM `prescriptions`
+                                        $prescriptionsSql = "SELECT * FROM `prescriptions`
                                                 WHERE `patient_id` = {$patient['id']} AND `doctor_id` = {$doc['id']}";
 
-                                            $prescriptionsResults = mysqli_query($mysqli, $prescriptionsSql);
-                                            $prescriptionsRows = mysqli_fetch_all($prescriptionsResults, MYSQLI_ASSOC);
+                                        $prescriptionsResults = mysqli_query($mysqli, $prescriptionsSql);
+                                        $prescriptionsRows = mysqli_fetch_all($prescriptionsResults, MYSQLI_ASSOC);
 
-                                            if (count($prescriptionsRows)) {
-                                                foreach ($prescriptionsRows as $prescription) {
-                                                    // var_dump($prescription);
-                                                    $formattedDate = date('m/d/Y', strtotime($prescription['date']));
+                                        if (count($prescriptionsRows)) {
+                                            foreach ($prescriptionsRows as $prescription) {
+                                                // var_dump($prescription);
+                                                $formattedDate = date('m/d/Y', strtotime($prescription['date']));
 
-                                                    echo "{$prescription['text']} - {$formattedDate}";
+                                                echo "{$prescription['text']} - {$formattedDate}";
 
-                                                    ?>
-                                                    </br>
-                                                        <button onclick="location.href='./editPrescription.php?prescId=<?php echo $prescription['id']?>'" type="button" class="edit-pres-btn">Edit</button>
-                                                        <form action="../src/php/deletePrescription.php" method="POST" style="float:right">
-                                                            <input type="hidden" value="<?php echo $prescription['id']?>" name="prescriptionId">
-                                                            <input type="submit" value="Delete" class="delete-pres-btn">
-                                                        </form>
-                                                    <?php
-                                                }
+                                        ?>
+                                                </br>
+                                                <button onclick="location.href='./editPrescription.php?prescId=<?php echo $prescription['id'] ?>'" type="button" class="edit-pres-btn">Edit</button>
+                                                <form action="../src/php/deletePrescription.php" method="POST" style="float:right">
+                                                    <input type="hidden" value="<?php echo $prescription['id'] ?>" name="prescriptionId">
+                                                    <input type="submit" value="Delete" class="delete-pres-btn">
+                                                </form>
+                                        <?php
                                             }
+                                        }
                                         ?>
                                         <br>
                                         <button onclick="location.href='./addPrescription.php'" type="button" class="add-pres-btn">Add</button>
-                                        </td>
-                                        
-                                        <td>
+                                    </td>
+
+                                    <td>
                                         <?php
-                                            $labTestsSql = "SELECT * FROM `lab_tests`
+                                        $labTestsSql = "SELECT * FROM `lab_tests`
                                                 WHERE `patient_id` = {$patient['id']} AND `doctor_id` = {$doc['id']}";
 
-                                            $labTestsResults = mysqli_query($mysqli, $labTestsSql);
-                                            $labTestsRows = mysqli_fetch_all($labTestsResults, MYSQLI_ASSOC);
+                                        $labTestsResults = mysqli_query($mysqli, $labTestsSql);
+                                        $labTestsRows = mysqli_fetch_all($labTestsResults, MYSQLI_ASSOC);
 
-                                            if (count($labTestsRows)) {
-                                                foreach ($labTestsRows as $labTest) {
-                                                    $formattedDate = date('m/d/Y', strtotime($labTest['date']));
-                                                    echo "{$labTest['lab_test_desc']} - {$formattedDate}";
-                                                    
-                                                    ?>
-                                                        <span>
-                                                            <button onclick="location.href='./editLabTest.php?labTestId=<?php echo $labTest['id']?>'" type="button" class="edit-lab-btn">Edit</button>
-                                                            <button onclick="location.href='./viewLabTest.php?labTestId=<?php echo $labTest['id']?>'" type="button" class="edit-lab-btn">View</button>
-                                                            <form action="../src/php/deleteLabTest.php" method="POST" style="float:right">
-                                                                <input type="hidden" value="<?php echo $labTest['id']?>" name="labTestId">
-                                                                <input type="submit" value="Delete"  class="delete-lab-btn">
-                                                            </form>
-                                                        </span>
-                                                    <?php
-                                                }
+                                        if (count($labTestsRows)) {
+                                            foreach ($labTestsRows as $labTest) {
+                                                $formattedDate = date('m/d/Y', strtotime($labTest['date']));
+                                                echo "{$labTest['lab_test_desc']} - {$formattedDate}";
+
+                                        ?>
+                                                <span>
+                                                    <button onclick="location.href='./editLabTest.php?labTestId=<?php echo $labTest['id'] ?>'" type="button" class="edit-lab-btn">Edit</button>
+                                                    <button onclick="location.href='./viewLabTest.php?labTestId=<?php echo $labTest['id'] ?>'" type="button" class="edit-lab-btn">View</button>
+                                                    <form action="../src/php/deleteLabTest.php" method="POST" style="float:right">
+                                                        <input type="hidden" value="<?php echo $labTest['id'] ?>" name="labTestId">
+                                                        <input type="submit" value="Delete" class="delete-lab-btn">
+                                                    </form>
+                                                </span>
+                                        <?php
                                             }
+                                        }
                                         ?>
                                         <br>
                                         <button onclick="location.href='./addPrescription.php'" type="button" class="add-pres-btn">Add</button>
-                                        </td>
-                                    </tr>
-                                <?php
+                                    </td>
+                                </tr>
+                        <?php
                             }
                         }
-                    ?>
-                </tbody>
+                        ?>
+                    </tbody>
             </div>
             </table>
         </section>
@@ -175,4 +194,5 @@ include '../src/php/dbconnect.php';
     <script defer type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
     <script defer src="../src/js/patientsDataTables.js"></script>
 </body>
+
 </html>
