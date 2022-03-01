b8437405b86a30a871992207aea6d32d18976c71
therealmai <jomar35leano@gmail.com>
2022-03-01 22:36:22 +0800

    fixed prescription

--- public/addPrescription.php
+++ public/addPrescription.php
@@ -1,67 +1,80 @@
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../src/css/profile.css">
-        <link rel="stylesheet" href="../src/css/addPrescriptionLabTest.css">
-        <title>SmartCare - Add Prescription</title>
-    </head>
-    <body>
-        <?php
-            include './session_check.php';
-            if ($_SESSION['currUser']['role'] != 'doctor') {
-                header('location: ./prescriptions.php');
-            }
-            include '../src/php/dbconnect.php';
-            include "./header.php"; ?>
-            <div class="form-body">
-                <table style="width:100%">
-                <col span="1" style="width: 50%;">
-                <col span="1" style="width: 50%;">
-                <form action="../src/php/createPrescription.php" method="post">
-                    <tr>
-                        <td><label for="patient">Patient</label></td>
-                        <td>
-                            <select name="patientId" id="patient">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/css/profile.css">
+    <link rel="stylesheet" href="../src/css/addPrescriptionLabTest.css">
+    <title>SmartCare - Add Prescription</title>
+</head>
+
+<body>
+    <?php
+    include './session_check.php';
+    if ($_SESSION['currUser']['role'] != 'doctor') {
+        header('location: ./prescriptions.php');
+    }
+    include '../src/php/dbconnect.php';
+    include "./header.php";
+    $query = "SELECT id FROM `doctors` WHERE userID = {$_SESSION['currUser']['id']}";
+    $doctor = mysqli_query($mysqli, $query);
+    $result = mysqli_fetch_assoc($doctor);
+    $query = "SELECT DISTINCT PatientID FROM `appointments` WHERE DoctorID = {$result["id"]}";
+    $patients = mysqli_query($mysqli, $query);
+    while ($result = mysqli_fetch_assoc($patients)) {
+        $row[] = $result;
+    } ?>
+    <div class="form-body">
+        <table style="width:100%">
+            <col span="1" style="width: 50%;">
+            <col span="1" style="width: 50%;">
+            <form action="../src/php/createPrescription.php" method="post">
+                <tr>
+                    <td><label for="patient">Patient</label></td>
+                    <td>
+                        <select name="patientId" id="patient">
                             <?php
-                                $patientsSql = "SELECT `p`.*, `u`.`firstname`, `u`.`lastname`, `u`.`middle_initial`
-                                    FROM `patients` `p`
-                                    LEFT JOIN `users` `u` ON `p`.`userID` = `u`.`id`";
-
-                                $patientsResults = mysqli_query($mysqli, $patientsSql);
-                                $patientsRows = mysqli_fetch_all($patientsResults, MYSQLI_ASSOC);
-
-                                if (count($patientsRows)) {
-                                    foreach($patientsRows as $patient) {
-                                        ?>
-                                            <option value="<?php echo $patient['id']?>"><?php echo "{$patient['firstname']} ".
-                                                                    (!empty($patient['middle_initial'])
-                                                                        ?"{$patient['middle_initial']}. "
-                                                                        :"")
-                                                                    ."{$patient['lastname']}"; ?></option>
-                                        <?php
+                            //SELECT * FROM `users` LEFT JOIN patients ON users.id = patients.userID WHERE patients.id = $row;
+                            if (isset($row)) {
+                                for ($x = 0; $x < count($row); $x++) {
+                                    $user = "SELECT users.firstname, users.middle_initial, users.lastname FROM `users` LEFT JOIN patients ON users.id = patients.userID WHERE patients.id = {$row[$x]['PatientID']}";
+                                    $userResults = mysqli_query($mysqli, $user);
+                                    while ($row1 = mysqli_fetch_assoc($userResults)) {
+                                        $patient[] = $row1;
                                     }
                                 }
+                            }
+
+                            for ($x = 0; $x < count($patient); $x++) {
+                            ?>
+                                <option value="<?php echo $patient['id'] ?>"><?php echo "{$patient[$x]["firstname"]} " .
+                                                                                    (!empty($patient[$x]['middle_initial'])
+                                                                                        ? "{$patient[$x]['middle_initial']}. "
+                                                                                        : "")
+                                                                                    . "{$patient[$x]['lastname']}"; ?></option>
+                            <?php
+                            }
                             ?>
                         </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="date">Date</label></td>
-                        <td><input type="date" name="date" id="date"></td>
-                    </tr>
-                    <tr>
-                        <td><label for="prescription">Prescription</label></td>
-                        <td><input type="text" name="prescription" id="prescription"></td>
-                    </tr>
-                    <td></td>
-                    <td>
-                    <button type="submit" class="add-form-btn">Add Prescription</button>
                     </td>
-                    
-                </form>
-                </table>
-                </div>
-    </body>
+                </tr>
+                <tr>
+                    <td><label for="date">Date</label></td>
+                    <td><input type="date" name="date" id="date"></td>
+                </tr>
+                <tr>
+                    <td><label for="prescription">Prescription</label></td>
+                    <td><input type="text" name="prescription" id="prescription"></td>
+                </tr>
+                <td></td>
+                <td>
+                    <button type="submit" class="add-form-btn">Add Prescription</button>
+                </td>
+
+            </form>
+        </table>
+    </div>
+</body>
+
 </html>
